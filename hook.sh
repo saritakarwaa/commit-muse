c93c1512322852a8e84ca6ac8e1b072ac407a09b
saritakarwaa <karwasarita672@gmail.com>
2025-07-21 21:15:04 +0530

    add commit-msg hook

--- hook.sh
+++ hook.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash  #shebang line:tells the system to run this script using the bash shell
 
 # commit-msg hook entry point
-COMMIT_MSG_FILE=$1
-ORIGINAL_MSG=$(cat "$COMMIT_MSG_FILE")
+COMMIT_MSG_FILE=$1 #git passes the path to the commit message file as the first argument to the commit-msg hook
+ORIGINAL_MSG=$(cat "$COMMIT_MSG_FILE") #stores the original commit msg
 STYLE="${BARD_STYLE:-Shakespeare}"
 echo "Using style: $STYLE"
 
@@ -15,7 +15,7 @@ fi
 # Gemini API endpoint
 ENDPOINT="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$GEMINI_API_KEY"
 
-#JSON payload
+#Creates a JSON payload using jq
 JSON_DATA=$(jq -n \
   --arg style "$STYLE" \
   --arg msg "$ORIGINAL_MSG" \
@@ -23,7 +23,7 @@ JSON_DATA=$(jq -n \
 
 
 
-# Call Gemini API
+# make post request to the gemini api
 REWRITTEN_MSG=$(curl -s -X POST "$ENDPOINT" \
   -H "Content-Type: application/json" \
   -d "$JSON_DATA" | jq -r '.candidates[0].content.parts[0].text')
